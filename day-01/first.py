c70d4906a1c1d26c8aa807fcef39890d352f6d0b
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-14 09:26:27 +0545

    upddated upto day-2

--- day-01/first.py
+++ day-01/first.py
@@ -1,4 +1,22 @@
 # Python with AI:
 
 # day-01:
-print("Hello there!")
+''' 
+-----------> doc string: 
+'''
+
+
+
+# print("Hello there!")
+# print("My name is Naresh Bohara.")
+# print("I am 24 years old.")
+# print("I am from Dhangadhi.")
+
+# print('''
+# 1. hello
+# 2. k xa
+#       3.  i am here
+# ''')
+'''
+print("hiiii") 
+'''
---
+++ day-02/Readme.md
@@ -0,0 +1,5 @@
+# 1. varaible
+
+# 2. dadta types
+
+# 3. operators
