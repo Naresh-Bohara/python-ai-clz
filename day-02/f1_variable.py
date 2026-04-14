5f246a09f7412a89bd0c0a92ed20caf820bc6172
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-14 09:46:53 +0545

    upddated upto day-2

---
+++ day-02/f1_variable.py
@@ -0,0 +1,13 @@
+# variable: it is container that store value for further processing.
+
+# variables demo:
+name = "Naresh Bohara"
+age  = 23
+address = "Dhangadhi"
+
+# print("My name is:",name, ", I am", age, ", I live in ", address)
+
+# print("My name is", name,"i am ", age,"I am from", address)
+
+# f-string
+print(f"My name is {name}. I am {age} years old. I am from {address}")
