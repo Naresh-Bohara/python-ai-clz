c79bfc597aa8513ef5d9bbaea62515358a93cdac
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-14 10:02:18 +0545

    upddated upto day-2

--- day-02/f1_variable.py
+++ day-02/f1_variable.py
@@ -1,6 +1,7 @@
 # variable: it is container that store value for further processing.
 
-# variables demo:
+#-------------- 1. variables demo: ------------------------
+
 name = "Naresh Bohara"
 age  = 23
 address = "Dhangadhi"
@@ -10,4 +11,28 @@ address = "Dhangadhi"
 # print("My name is", name,"i am ", age,"I am from", address)
 
 # f-string
-print(f"My name is {name}. I am {age} years old. I am from {address}")
+# print(f"My name is {name}. I am {age} years old. I am from {address}")
+
+# -------------------- 2. some rules for variable declaration:  ------------------------
+# 1. Must start with letter or underscore
+# 9name="naresh"
+# _2name = "Naresh"
+# print(_2name)
+
+# 2. variables are case sensitive:
+NAME = "Naresh"
+name = "Dammar"
+print(NAME, name)
+
+# 3. avoid use of predefind words in python:
+if1 = 23
+class2  = 43
+while3 = 0
+
+# -----------------3. Variable Assignment: ------------------------
+# -> assign value to variable.
+age = 23
+
+# multiple varaiable assignment:
+stname, stroll, stclz = "Dammar", 12, "KMC"
+print(stname, stroll, stclz)
