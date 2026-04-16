eea0a11ce94315f25931a8acf1b922aa8539d7a6
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-16 09:59:46 +0545

    updated

---
+++ day-02/f4_operators.py
@@ -0,0 +1,61 @@
+# operators:
+# 1. Arithmetic operators:
+# a= 23
+# b=4
+# print(a+b) # 27
+# print(a-b) # 19
+# print(a*b) # 72
+# print(a/b) #5.75
+# print(a//b) # floor value-- 5
+# print(a%b)  # 3
+# print(a**b) # 23*23*23*23
+
+# question: 23+ 45 /2*4//3**5*6+(2**3)
+
+# print( 23+ 45 /2*4//3**5*6+(2**3))
+
+# operator precedance rule: (PEDMAS rule)
+# 1. parenthesis
+# 2. ** expotential
+# 3. *, /,//, %, 
+# 4. +, -
+
+'''
+print( 23+ 45 /2*4//3**5*6+(2**3))
+
+print( 23+ 45 /2*4//3**5*6+8 )
+
+print( 23+ 45 /2*4//243*6+8 )
+
+print( 23+ 45/8//1458+8 )
+
+print( 23+ 5.625//1458+8 )
+
+print( 23+ 0 +8 )
+
+--31
+
+'''
+
+# 2. comparision  (relational operators):
+# x= 10
+# y= 9
+
+# print(x==y)
+# print(x!=y)
+# print(x > y)
+# print(y < x)
+# print(y >= x)
+# print(y <= x)
+
+# 3. logical operators:
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
+
+
+
+
